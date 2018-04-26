820f3c2f8ad34d77797051060e496415696023f8
MonicaSelvaraj <mselvaraj@wisc.edu>
2018-04-26 14:59:09 -0500

    PC1 through vasa and dazl

--- LineOfBestFit/LineThroughPoints.py
+++ LineOfBestFit/LineThroughPoints.py
@@ -48,56 +48,61 @@ X2 = numpy.array(X2); Y2 = numpy.array(Y2); Z2 = numpy.array(Z2); S2 = numpy.arr
 X1 = X1.astype(float); Y1= Y1.astype(float); Z1= Z1.astype(float); S1= S1.astype(float)
 X2 = X2.astype(float); Y2= Y2.astype(float); Z2 = Z2.astype(float); S2= S2.astype(float)
 
-#Plotting the SMA's to see if the helix wraps around PC1
-def movingaverage(values, window):
-    weights = numpy.repeat(1.0, window)/window
-    #valid only runs the sma's on valid points
-    smas = numpy.convolve(values, weights, 'valid')
-    return smas #returns a numpy array
-
-#Generating a plot of the original points
-ax = fig.add_subplot(1,1,1, projection = '3d')
+#Generating a plot of the original points - both vasa and dazl
+ax = fig.add_subplot(1,3,1, projection = '3d')
 ax.grid(False)
 red = ax.scatter (X1, Y1, Z1, c = 'r', marker='o',linewidths=2)
-#green = ax.scatter (X2, Y2, Z2, c = 'g', marker='o',linewidths=2)
-#ax.set_title('unclustered')
-#ax.legend((red, green),
-#           ('vasa', 'dazl'),
-#           scatterpoints=1,
-#           loc='best',
-#           ncol=1,
-#           fontsize=8)
+green = ax.scatter (X2, Y2, Z2, c = 'g', marker='o',linewidths=2)
+ax.set_title('GP-RNP')
+ax.legend((red, green),
+           ('vasa', 'dazl'),
+           scatterpoints=1,
+           loc='best',
+           ncol=1,
+           fontsize=8)
 ax.set_xlabel ('x, axis')
 ax.set_ylabel ('y axis')
 ax.set_zlabel ('z axis')
 
-data = numpy.concatenate((X1[:, numpy.newaxis], 
-                       Y1[:, numpy.newaxis], 
-                       Z1[:, numpy.newaxis]), 
+
+def PC1(X,Y,Z):
+        data = numpy.concatenate((X[:, numpy.newaxis], 
+                       Y[:, numpy.newaxis], 
+                       Z[:, numpy.newaxis]), 
                       axis=1)
-print(data)
+        print(data)
+        datamean = data.mean(axis=0)
+        print (datamean) #This is going to be the center of my helix
+        ax.scatter (datamean[0], datamean[1], datamean[2], c = 'b', marker='o')
+        uu, dd, vv = numpy.linalg.svd(data - datamean)
+        #Taking the variation in the z dimension, because this is the dimension of PC1
+        #Linear algebra - figure out what exactly is happening in terms of dimensional collapsation
+        linepts = vv[0] * numpy.mgrid[-20:20:2j][:, numpy.newaxis]
+        #Moving the line to be in between the points
+        linepts += datamean
+        return linepts;
 
-datamean = data.mean(axis=0)
-print (datamean) #This is going to be the center of my helix
-ax.scatter (datamean[0], datamean[1], datamean[2], c = 'b', marker='o')
+#Generating a subplot of vasa-C1 and drawing PC1
+ax = fig.add_subplot(1,3,2, projection = '3d')
+ax.grid(False)
+red = ax.scatter (X1, Y1, Z1, c = 'r', marker='o',linewidths=2)
+ax.set_title('vasa PC1')
+ax.set_xlabel ('x, axis')
+ax.set_ylabel ('y axis')
+ax.set_zlabel ('z axis')
+linePointsC1 = PC1(X1,Y1,Z1)
+ax.plot3D(*linePointsC1.T)
 
-uu, dd, vv = numpy.linalg.svd(data - datamean)
-
-#Taking the variation in the z dimension, because this is the dimension of PC1
-#Linear algebra - figure out what exactly is happening in terms of dimensional collapsation
-linepts = vv[0] * numpy.mgrid[-20:20:2j][:, numpy.newaxis]
-
-#Moving the line to be in between the points 
-linepts += datamean
-
-#Why are we taking the transpose?
-ax.plot3D(*linepts.T)
-
-#SMA doesn't seem to show anything, trying PC1
-#xline = movingaverage(movingaverage(X1, 10),5)
-#yline = movingaverage(movingaverage(Y1, 10),5)
-#zline = movingaverage(movingaverage(Z1, 10),5)
-#ax.plot3D(xline,yline,zline,'red')
+#Generating a subplot of vasa-C1 and drawing PC1
+ax = fig.add_subplot(1,3,3, projection = '3d')
+ax.grid(False)
+green = ax.scatter (X2, Y2, Z2, c = 'g', marker='o',linewidths=2)
+ax.set_title('dazl PC1')
+ax.set_xlabel ('x, axis')
+ax.set_ylabel ('y axis')
+ax.set_zlabel ('z axis')
+linePointsC2 = PC1(X2,Y2,Z2)
+ax.plot3D(*linePointsC2.T)
 
 plt.show()
 
