57e941155b7ac73f06f88071abfeb07213272b4f
MonicaSelvaraj <mselvaraj@wisc.edu>
2018-05-03 11:15:30 -0500

    Using 2D fit's to get an overall fit

--- CurveFitting/CurveFittingUsingPCs.py
+++ CurveFitting/CurveFittingUsingPCs.py
@@ -18,7 +18,7 @@ from scipy.optimize import curve_fit
 
 sys.setrecursionlimit(10000)
 fig = plt.figure( )
-plt.style.use('dark_background')
+#plt.style.use('dark_background')
 
 #Original data 
 X1 = list(); Y1 = list(); Z1 = list(); S1 = list()#C1 - red
@@ -38,7 +38,7 @@ with open ('deconvolutedAC2.csv', 'r') as csv_file:
     csv_reader = csv.reader (csv_file)
     for line in csv_reader:
         #each line has X,Y,Z, radius
-            if (float(line[1]) > 10):
+            if (float(line[1]) > 15): 
                     X2.append(line[0])
                     Y2.append(line[1])
                     Z2.append(line[2])
@@ -80,48 +80,63 @@ def PC1(X,Y,Z):
         #Taking the variation in the z dimension, because this is the dimension of PC1
         #Linear algebra - figure out what exactly is happening in terms of dimensional collapsation
         linepts = vv[0] * numpy.mgrid[-20:20:2j][:, numpy.newaxis]
-        #print (vv[0])
-       # print (vv[1])
-        #print (vv[2])
+        print (vv[0])
+        print (vv[1])
+        print (vv[2])
         #Moving the line to be in between the points
         linepts += datamean
         return linepts;
-
-
+'''
+[27.23232954 24.56108827  9.5954606 ]
+[ 0.41980803  0.74475744 -0.51874616]
+[-0.90745483  0.35508816 -0.22458437]
+[0.01693974 0.56502102 0.82490259]
+[26.27468425 24.7928729  10.52890996]
+[ 0.60816783  0.61608871 -0.50056628]
+[ 0.7237724  -0.68934573  0.03091892]
+[-0.32601442 -0.38109995 -0.8651459 ]
+'''
 Points = list(zip(X1,Y1,Z1))
-center = numpy.array([27.61704192, 25.67052383,  8.83293281])#Vasa - C1
-Pc1 = numpy.array([ 0.42995246,  0.74540579, -0.50942231])
-Pc2 = numpy.array([ 0.90214046, -0.33230552,  0.27516473])
-Pc3 = numpy.array([ 0.03582554, -0.57787823, -0.8153363 ])
+center = numpy.array([27.23232954, 24.56108827,  9.5954606 ])#Vasa - C1
+Pc1 = numpy.array([ 0.41980803,  0.74475744, -0.51874616])
+Pc2 = numpy.array([-0.90745483,  0.35508816, -0.22458437])
+Pc3 = numpy.array([0.01693974, 0.56502102, 0.82490259])
 #If we don't sort - it might looks like a bunch of scribbles - sorted draws a nice line
 Points.sort(key=lambda i: numpy.dot(Pc1, i)) # Sorts by first PC so it draws lines nicely
 C1s = numpy.dot(Points - center, Pc1) # Components in first PC direction
 C2s = numpy.dot(Points - center, Pc2) 
 C3s = numpy.dot(Points - center, Pc3)
-plt.scatter(C1s, C2s) # Shows plot without first PC
+plt.grid('true')
+#plt.scatter(C2s, C3s) # Shows plot without first PC
+plt.scatter(C2s, C1s)
+#plt.scatter(C1s, C3s)
 plt.show()
 
 def helixFit(pc1, r, frequency, phase):
     return r*numpy.cos(pc1*frequency + phase) #Doesn't matter if it's sin or cos
 
 # Need to fit each of the components separately
-popt, pcov = curve_fit(helixFit, C1s, C2s, p0=[10, 0.5, -numpy.pi/2]) # Predicts C2 given C1
+popt, pcov = curve_fit(helixFit, C1s, C2s, p0=[5, 0.2, -numpy.pi/2]) # Predicts C2 given C1
 print("Fit parameters (radius, frequency, phase) for C1 -> C2:", popt)
 C2Ps = [helixFit(c1, *popt) for c1 in C1s]
 
-popt, pcov = curve_fit(helixFit, C1s, C3s, p0=[10, 0.5, 0]) # Predicts C3 given C1
+popt, pcov = curve_fit(helixFit, C1s, C3s, p0=[5, 0.2, 0]) # Predicts C3 given C1
 print("Fit parameters (radius, frequency, phase) for C1 -> C3:", popt)
 C3Ps = [helixFit(c1, *popt) for c1 in C1s]
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter3D(X1, Y1, Z1)
+ax.scatter3D(X1, Y1, Z1, c = 'r', marker='o')
 # Plot lines connecting nearby points in Helix
 #Drawing a line throught the middle of C1, C2Ps, C3Ps
 for i in range(len(C1s)-1): #Go from the center into the PC's direction by this much for each point
         start = center + C1s[i]*Pc1 + C2Ps[i]*Pc2 + C3Ps[i]*Pc3
         end = center + C1s[i+1]*Pc1 + C2Ps[i+1]*Pc2 + C3Ps[i+1]*Pc3
         ax.plot3D([start[0], end[0]], [start[1], end[1]], [start[2], end[2]], c = 'blue')
+plt.show()
+
+plt.scatter(C1s, C2s) # True
+plt.plot(C1s, C2Ps) # Fit
 
 #Plot3D, takes a start x, end x, start y, end y, start 
 
@@ -158,10 +173,5 @@ ax.plot3D(*linePointsC2.T)
 plt.show()
 '''
 
-
-
-    
-    
-    
 
 
