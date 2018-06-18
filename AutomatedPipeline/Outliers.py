76ae09f53b429a9061c39935c7a66168b364660a
MonicaSelvaraj <mselvaraj@wisc.edu>
2018-06-17 19:05:30 -0500

    Plot styling

---
+++ AutomatedPipeline/Outliers.py
@@ -0,0 +1,94 @@
+'''
+- Plots of C1s, C2s, C3s before outliers are removed
+- Remove outliers for each channel individually
+- Plots of C1s, C2s, and C3s after outliers have been removed, with lines to indicate boundaries
+of removal
+'''
+
+#!/usr/bin/python
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+from matplotlib.axes import Axes
+import scipy.stats
+import statistics
+import numpy 
+import csv
+import math
+from matplotlib import style
+from scipy.spatial import distance 
+
+plt.style.use('dark_background')
+
+#Original data 
+C1r = list(); C2r = list(); C3r = list()#C1 - red
+C1g = list(); C2g = list(); C3g = list()#C2 - green
+
+#Reading in the data 
+with open ('ComponentsC1.csv', 'r') as csv_file:
+    csv_reader = csv.reader (csv_file)
+    for line in csv_reader:
+            C1r.append(line[0])
+            C2r.append(line[1])
+            C3r.append(line[2])
+            
+with open ('ComponentsC2.csv', 'r') as csv_file:
+    csv_reader = csv.reader (csv_file)
+    for line in csv_reader:
+            C1g.append(line[0])
+            C2g.append(line[1])
+            C3g.append(line[2])
+        
+       
+C1r = numpy.array(C1r); C2r = numpy.array(C2r); C3r = numpy.array(C3r)
+C1g = numpy.array(C1g); C2g = numpy.array(C2g); C3g = numpy.array(C3g)
+C1r = C1r.astype(float); C2r= C2r.astype(float); C3r = C3r.astype(float)
+C1g = C1g.astype(float); C2g= C2g.astype(float); C3g = C3g.astype(float)       
+
+#Shows plots before removing outliers
+plt.scatter(C2g, C3g, c='g'); plt.scatter(C2r, C3r, c='r'); plt.title('C2 vs C3');  plt.ylim(-20, 20); plt.xlim(-20,20); plt.show() # Shows plot without first PC
+plt.scatter(C1g, C2g, c='g'); plt.scatter(C1r, C2r, c='r'); plt.title('C1 vs C2');  plt.ylim(-20, 20); plt.xlim(-20,20); plt.show() # Shows plot without third PC
+plt.scatter(C1g, C3g, c='g'); plt.scatter(C1r, C3r, c='r'); plt.title(' C1 vs C3');  plt.ylim(-20, 20); plt.xlim(-20,20); plt.show()# Shows plot without second PC
+
+#REMOVING OUTLIERS
+def removeOutliers(C1, C2, C3):
+        dst = list()
+        #Finding distances of points from (0,0)
+        for x,y in zip (C2, C3):
+                a = (x,y)
+                b = (0,0)
+                dst.append(distance.euclidean(a,b))
+
+        dst = numpy.array(dst); dst = dst.astype(float)
+        
+        #Plotting the distances from the origin to check the distribution
+        #plt.hist(dst) #Auto binned histogram
+        #plt.show()
+ 
+        meanDst = numpy.mean(dst, axis = 0); print (meanDst)
+        sdDst = numpy.std(dst, axis = 0); print (sdDst)
+
+        #outliersIndex contains the positions of the outliers
+        outliersIndex = numpy.where(dst > meanDst + 2 * sdDst)
+        print (outliersIndex)
+        #Removing the outliers from C1, C2, C3
+        newC1 = numpy.delete (C1, outliersIndex)
+        newC2 = numpy.delete (C2, outliersIndex)
+        newC3 = numpy.delete (C3, outliersIndex)
+        return (newC1, newC2, newC3);
+
+CleanedChannel1 = removeOutliers (C1r, C2r, C3r)
+CleanedChannel2 = removeOutliers (C1g, C2g, C3g)
+
+#Writing cleaned data to a new file
+numpy.savetxt("CleanedComponentsC1.csv", numpy.column_stack((CleanedChannel1[0], CleanedChannel1[1], CleanedChannel1[2])), delimiter=",", fmt='%s')
+numpy.savetxt("CleanedComponentsC2.csv", numpy.column_stack((CleanedChannel2[0], CleanedChannel2[1], CleanedChannel2[2])), delimiter=",", fmt='%s')
+
+#Shows plots after removing outliers
+plt.scatter(CleanedChannel2[2], CleanedChannel2[1], c='g'); plt.scatter(CleanedChannel1[2],CleanedChannel1[1],c='r');plt.title('C2 vs C3'); plt.ylim(-20, 20); plt.xlim(-20,20); plt.show() # Shows plot without first PC
+plt.scatter(CleanedChannel2[0], CleanedChannel2[1], c='g'); plt.scatter(CleanedChannel1[0], CleanedChannel1[1], c='r'); plt.title('C1 vs C2'); plt.ylim(-20, 20); plt.xlim(-20,20); plt.show() # Shows plot without third PC
+plt.scatter(CleanedChannel2[0], CleanedChannel2[2], c='g');plt.scatter(CleanedChannel1[0], CleanedChannel1[2],  c='r'); plt.title(' C1 vs C3'); plt.ylim(-20, 20); plt.xlim(-20,20); plt.show()# Shows plot without second PC
+
+
+
+
